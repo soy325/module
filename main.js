8c0eddc40be210e5a34f86bb94fc81461bd54737
soy325 <soy325@github.com>
2024-11-21 16:44:37 +0900

    02. header 완성

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+import {menu} from"./src/js/menu.js"
+import {link} from"./src/js/link.js"
+
+window.addEventListener('load',function(){
+    menu()
+})
+window.addEventListener('load',function(){
+    link()
+})
