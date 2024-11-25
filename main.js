835059b1d1a3ebb48f8606db8c6a6bdf33d8bcb8
soy325 <soy325@github.com>
2024-11-25 15:37:22 +0900

    bgcolor 수정

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
 import {menu} from"./src/js/menu.js"
 import {link} from"./src/js/link.js"
+import{bgColor} from"./src/js/bgColor.js"
 
 window.addEventListener('load',function(){
     menu()
-})
-window.addEventListener('load',function(){
     link()
+    bgColor()
+
 })
